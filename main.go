0b856bf4ec18933494582e67252770c519ba406b
Brunosg9 <brunog.silva@live.com>
2024-01-23 19:59:42 -0300

    commit with pipeline

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webContent = "dev-ops-ninja:v99"
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, webContent)
+}
